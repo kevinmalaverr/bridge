c9bb78dc9a7c6ed4fee24d712fae7ba4523a07b4
kevinmalaverr <kevinmalaverr@gmail.com>
2021-04-08 19:51:39 -0500

    added middlewares

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	server := NewServer(":3000")
+	server.Handle("/", HandleRoot)
+	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth()))
+	server.Handle("/user", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "inline function")
+	})
 	server.Listen()
 }
--- server.go
+++ server.go
@@ -22,3 +22,15 @@ func (s *Server) Listen() error {
 	}
 	return nil
 }
+
+func (s *Server) Handle(path string, handler http.HandlerFunc) {
+	s.router.rules[path] = handler
+}
+
+func (s *Server) AddMiddleware(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		// pass handler to each middleware
+		f = m(f)
+	}
+	return f
+}
